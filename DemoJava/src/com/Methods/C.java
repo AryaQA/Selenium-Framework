1aa999e351f18c3b399c281cfefd610ee704df7d
Abhishek kumar arya <abhishek.kr.arya004@gmail.com>
2025-08-06 23:24:33 +0530

    methods

---
+++ DemoJava/src/com/Methods/C.java
@@ -0,0 +1,35 @@
+package com.Methods;
+
+ public class C {
+
+	public int k = 200;
+
+
+	public void div(int a, int b ) {
+
+
+		int c = a + b;
+
+		System.out.println(c);
+
+	}
+
+	public void display() {
+
+
+		System.out.println("Hello");
+
+
+
+	}
+
+	public void add(int a, int b ) {
+
+
+		int c = a + b;
+
+		System.out.println(c);
+
+	}
+
+}
